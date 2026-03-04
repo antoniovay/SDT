9d6bc2401d7d9b14ad2c0a3dfe2418f6b3f74c25
Antoniovay <miranton94@gmail.com>
2026-03-04 11:42:58 +0700

    Test commit

---
+++ Lab 1 Files Watching/Lab 1 Files Watching/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 1 Files Watching
+//
+//  Created by Antony Miroshnichenko on 04.03.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
